9841d5ebefe956850cc69979a511a2c38162067a
djwford <derekweatherford@gmail.com>
2015-12-23 21:33:54 +0000

    precompiled

---
+++ public/assets/course_metafiles-9cc1a1e6c93883f0fb8e18f8c1af075f0074b70b22dcd4545306408183ef7a08.js
@@ -0,0 +1,27 @@
+Opentip.defaultStyle = "dark";
+$(document).ready(function(){
+  // perform basic validations before submission
+  var fields = $("#courseTitle, #author, #course_metafile_short_description, #course_metafile_description, #moduleCount, #courseID");
+  $("input:submit").click(function(){
+    // checks for presence
+    for(var i=0; i < fields.length; i++){
+      if($(fields[i]).val() == ""){
+        event.preventDefault();
+        $(fields[i]).css({'background-color': '#FFAC65'});
+        alert("Please complete the highlighted items.");
+        break;
+      }
+    }
+    // strip newlines from descriptions
+    var descriptions = $("#course_metafile_description, #course_metafile_short_description")
+    for(var i=0; i < descriptions.length; i++){
+      var currentDescription = $(descriptions[i]).val();
+      $(descriptions[i]).val((currentDescription).replace(/\n/g," "));
+    }
+  });
+
+  // clear validations error formatting when any field is clicked
+  $(fields).click(function(){
+    $(this).css({"background-color": "#FFFFFF"})
+  });
+});

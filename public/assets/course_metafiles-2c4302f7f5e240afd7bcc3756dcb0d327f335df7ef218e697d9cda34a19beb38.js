e17936a2cf397abd860233ae50f59a4390cb516e
derek <derekweatherford@gmail.com>
2016-05-24 16:35:30 +0000

    precopile

--- public/assets/course_metafiles-2c4302f7f5e240afd7bcc3756dcb0d327f335df7ef218e697d9cda34a19beb38.js
+++ public/assets/course_metafiles-2c4302f7f5e240afd7bcc3756dcb0d327f335df7ef218e697d9cda34a19beb38.js
@@ -1,10 +1,12 @@
+Opentip.defaultStyle = "dark";
 
 $(document).ready(function(){
+
   // perform basic validations before submission
   var fields = $("#courseTitle, #author, #course_metafile_short_description, #course_metafile_description, #moduleCount, #courseID");
   $("input:submit").click(function(){
     // checks for presence
-    for(var i=0; i <= fields.length; i++){
+    for(var i=0; i < fields.length; i++){
       if($(fields[i]).val() == ""){
         event.preventDefault();
         $(fields[i]).css({'background-color': '#FFAC65'});
@@ -14,7 +16,7 @@ $(document).ready(function(){
     }
     // strip newlines from descriptions
     var descriptions = $("#course_metafile_description, #course_metafile_short_description")
-    for(var i=0; i <= descriptions.length; i++){
+    for(var i=0; i < descriptions.length; i++){
       var currentDescription = $(descriptions[i]).val();
       $(descriptions[i]).val((currentDescription).replace(/\n/g," "));
     }
@@ -23,5 +25,22 @@ $(document).ready(function(){
   // clear validations error formatting when any field is clicked
   $(fields).click(function(){
     $(this).css({"background-color": "#FFFFFF"})
+  });
+
+  // enable chosen
+  $(".chosen-select").chosen({width: "100%"});
+
+  // suggested tags hide/reveal
+  $(".suggested_input").hide();
+  $("#suggest_button").click(function(){
+    var hidden_div = $(".suggested_input:hidden:first");
+    $(hidden_div).slideToggle("fast");
+    $(hidden_div).find("input:text:first").focus();    
+  });
+  $(".close_button").click(function(){
+    $(this).closest(".suggested_input").slideToggle('slow');
+    $(this).closest('.suggested_input').find("input:text").val('');
+    $(this).closest('.suggested_input').find(".tag_type_input").val('');
+
   });
 });
